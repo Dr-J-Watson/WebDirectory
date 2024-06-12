81a50a243f8e31f2c3b86f359ad960acca5c2e97
Okiles <colsonlenny5@gmail.com>
2024-06-12 17:36:07 +0200

    Person Details

--- WebDir.app/webdirapp/lib/screens/person_preview.dart
+++ WebDir.app/webdirapp/lib/screens/person_preview.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:webdirapp/models/person.dart';
 import 'package:webdirapp/screens/person_provider.dart';
+import 'package:webdirapp/screens/person_details.dart';
 
 class PersonPreview extends StatefulWidget {
   final Person person;
@@ -15,16 +16,29 @@ class PersonPreview extends StatefulWidget {
 class _PersonPreviewState extends State<PersonPreview> {
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: ListTile(
-        leading: CircleAvatar(
-          backgroundImage: widget.person.image != null
-              ? NetworkImage(widget.person.image!)
-              : null,
-        ),
-        title: Text('${widget.person.firstName} ${widget.person.lastName}'),
-        subtitle: Text(widget.person.numBureau),
+    return GestureDetector(
+    child: ListTile(
+      leading: CircleAvatar(
+        backgroundImage: widget.person.image != null
+            ? NetworkImage(widget.person.image!)
+            : null,
+        child: widget.person.image == null
+            ? Text(widget.person.firstName[0] + widget.person.lastName[0])
+            : null,
       ),
+      title: Text('${widget.person.firstName} ${widget.person.lastName}'),
+      subtitle: Text(widget.person.numBureau),
+      shape: const Border(bottom: BorderSide(color: Colors.grey)),
+    ),
+    onTap: () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => PersonDetails(person: widget.person),
+        ),
+      );
+    },
+    
     );
   }
 }
