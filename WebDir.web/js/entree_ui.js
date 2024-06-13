9cc854b5dab53c07d19020bb6674a6a2299f62ff
Vincent GEORGES <vincent.georges3@etu.univ-lorraine.fr>
2024-06-13 11:41:28 +0200

    supression lignes inutiles

--- WebDir.web/js/entree_ui.js
+++ WebDir.web/js/entree_ui.js
@@ -1,10 +1,8 @@
 import Handlebars from 'handlebars';
-import conf from "./config.js";
 import loader from "./loader.js";
 
 function displayEntree(data) {
     loader.loadEntree(data.id).then(entree => {
-        const urlserv = conf.urlserv;
         const container = document.getElementById('#main');
         const templateSource = document.querySelector('#entreeTemplate').innerHTML;
         const template = Handlebars.compile(templateSource);
