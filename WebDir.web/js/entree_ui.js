df0645a2ddfee9c071827d331326b32c08250497
Vincent GEORGES <vincent.georges3@etu.univ-lorraine.fr>
2024-06-13 11:35:08 +0200

    utilisation des loaders

--- WebDir.web/js/entree_ui.js
+++ WebDir.web/js/entree_ui.js
@@ -1,13 +1,16 @@
 import Handlebars from 'handlebars';
 import conf from "./config.js";
+import loader from "./loader.js";
 
 function displayEntree(data) {
-    const urlserv = conf.urlserv;
-    const container = document.getElementById('#main');
-    const templateSource = document.querySelector('#entreeTemplate').innerHTML;
-    const template = Handlebars.compile(templateSource);
-    const html = template(data);
-    container.innerHTML = html;
+    loader.loadEntree(data.id).then(entree => {
+        const urlserv = conf.urlserv;
+        const container = document.getElementById('#main');
+        const templateSource = document.querySelector('#entreeTemplate').innerHTML;
+        const template = Handlebars.compile(templateSource);
+        const html = template(entree);
+        container.innerHTML = html;
+    });
 }
 
 export default { displayEntree };
--- WebDir.web/js/service_ui.js
+++ WebDir.web/js/service_ui.js
@@ -1,13 +1,16 @@
 import Handlebars from 'handlebars';
 import conf from "./config.js";
+import loader from "./loader.js";
 
-function displayService(data) {
-    const urlserv = conf.urlserv;
-    const container = document.getElementById('#main');
-    const templateSource = document.querySelector('#serviceTemplate').innerHTML;
-    const template = Handlebars.compile(templateSource);
-    const html = template(data);
-    container.innerHTML = html;
+async function displayService(data) {
+    loader.loadService(data.id).then(service => {;
+        const urlserv = conf.urlserv;
+        const container = document.getElementById('#main');
+        const templateSource = document.querySelector('#serviceTemplate').innerHTML;
+        const template = Handlebars.compile(templateSource);
+        const html = template(service);
+        container.innerHTML = html;
+    });
 }
 
 export default { displayService };
