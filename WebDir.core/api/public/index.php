89082ca40e507d1517081cb39dffe7af7f52d57e
Dr-J-Watson <ptibandi54@gmail.com>
2024-06-14 11:18:52 +0200

    index

--- WebDir.core/api/public/index.php
+++ WebDir.core/api/public/index.php
@@ -7,7 +7,7 @@ require_once __DIR__ . '/../src/vendor/autoload.php';
 
 /* application boostrap */
 $app = require_once __DIR__ . '/../src/conf/bootstrap.php';
-Eloquent::init(__DIR__ . '/../src/conf/gift.db.conf.ini');
+Eloquent::init(__DIR__ . '/../src/conf/webdir.db.conf.ini');
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
