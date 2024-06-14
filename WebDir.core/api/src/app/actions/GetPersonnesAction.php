8734676ca12c6b82a4006be2187db97cce7ee26e
Dr-J-Watson <ptibandi54@gmail.com>
2024-06-14 11:06:30 +0200

    api

---
+++ WebDir.core/api/src/app/actions/GetPersonnesAction.php
@@ -0,0 +1,11 @@
+<?php 
+
+namespace WebDir\core\api\app\actions;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class GetPersonneAction extends AbstractAction
+{
+
+}
