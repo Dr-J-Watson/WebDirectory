ee9b4caca6566e318f8c9923645b2cc8feb1ba28
Okiles <colsonlenny5@gmail.com>
2024-06-14 15:31:22 +0200

    Adding .env file

--- WebDir.app/webdirapp/lib/main.dart
+++ WebDir.app/webdirapp/lib/main.dart
@@ -1,11 +1,15 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:webdirapp/screens/entry_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'web_dir_app.dart';
 
+
 Future<void> main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: "./lib/assets/.env");
 
   runApp(ChangeNotifierProvider(
     create: (context) => EntryProvider(),
--- WebDir.app/webdirapp/lib/screens/entry_provider.dart
+++ WebDir.app/webdirapp/lib/screens/entry_provider.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/material.dart';
 import 'package:webdirapp/models/entry.dart';
 import 'package:http/http.dart' as http;
@@ -21,7 +21,8 @@ class EntryProvider extends ChangeNotifier {
       final entriesdata = json['entrees'];
       
       for (var entry in entriesdata) {
-        final entryResponse = await http.get(Uri.parse('http://docketu.iutnc.univ-lorraine.fr:54002${entry['links']['self']['href']}'));
+        final url = Uri.parse('${dotenv.env['BASE_URL']}${dotenv.env['PORT']}${entry['links']['self']['href']}');
+        final entryResponse = await http.get(url);
           if (entryResponse.statusCode != 200) {
             throw Exception('Impossible de charger les Entrynes (code d\'erreur : ${response.statusCode})');
           } else {
